74f0daff41254c50f42ef0c8d90a948a11e12601
mikedev99 <mikedev035@gmail.com>
2026-05-18 00:47:36 +0800

    5

--- server/prisma-store.js
+++ server/prisma-store.js
@@ -1,4 +1,4 @@
-import { prisma } from "../lib/prisma.ts";
+import { prisma } from "../lib/prisma.js";
 import { reseedPrisma } from "../prisma/seed-data.js";
 import { normalizeClient } from "../src/data/clients.js";
 import { TIERS } from "../src/data/clients.js";
