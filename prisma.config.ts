4b56212b61d917a247b185618b32801dcaa8de18
mikedev99 <mikedev035@gmail.com>
2026-05-20 21:43:31 +0800

    7

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
     seed: "node prisma/seed.js",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DIRECT_URL || env("DATABASE_URL"),
   },
 });
